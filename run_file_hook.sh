ed4d594e7b779beeb6e618c1af5e778c4ff2a84c
Pekka Väänänen <pekka.vaananen@iki.fi>
2025-07-30 18:28:24 +0300

    read ELF from command line

--- run_file_hook.sh
+++ run_file_hook.sh
@@ -6,7 +6,7 @@
 # Usage: ./run_file_hook.sh testfiles
 set -o nounset
 GDB=gdb-multiarch
-ELF=build/example.elf # Your ROM's .elf file for symbols
+ELF=$2 # Your ROM's .elf file for symbols
 while true; do
     GDB_FILE_HOOK_DIR=$1 \
     $GDB --batch -ex "target remote localhost:9123" -ex "file ${ELF}" -x file_hook.py -ex "continue"
